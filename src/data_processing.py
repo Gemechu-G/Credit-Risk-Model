5bc0695e7960321e0ca9f9c68ca8a83ccd2ec9f3
gemechu-g <gemegudisa60@gmail.com>
2025-06-30 00:29:22 +0300

    Initial commit with project structure and README

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder # MinMaxScaler is an option too
